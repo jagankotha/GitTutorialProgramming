ec205bfb87ebd94ebf254fbdfe83c325aeea64bf
jagan353 <jaganmohanreddyk19@gmail.com>
2021-12-04 14:59:14 +0530

    second commit

---
+++ GitTutorialProgramming/src/main/java/com/example/GitTutorialProgramming/GitTutorialProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitTutorialProgramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTutorialProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTutorialProgrammingApplication.class, args);
+	}
+
+}

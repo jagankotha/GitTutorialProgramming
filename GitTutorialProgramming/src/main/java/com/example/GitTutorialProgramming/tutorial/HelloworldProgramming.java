6880ca72cd74fced394bc4f41bc2502a2ae2d2d2
jagan353 <jaganmohanreddyk19@gmail.com>
2021-12-04 16:08:09 +0530

    method added

---
+++ GitTutorialProgramming/src/main/java/com/example/GitTutorialProgramming/tutorial/HelloworldProgramming.java
@@ -0,0 +1,8 @@
+package com.example.GitTutorialProgramming.tutorial;
+
+public class HelloworldProgramming {
+
+    public void display(){
+        System.out.println ("this is the display world programming......." );
+    }
+}
